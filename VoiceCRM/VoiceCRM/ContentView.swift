54eae61f1bd55e07778b93999d015d8b28f86a1a
RitaPinto1230205 <1230205@isep.ipp.pt>
2026-03-14 16:58:56 +0000

    pipeline básico + ASR com SFSpeechRecognizer + read me

---
+++ VoiceCRM/VoiceCRM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceCRM
+//
+//  Created by Rita Pinto on 14/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceCRM/VoiceCRM/VoiceCRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCRMApp.swift
+//  VoiceCRM
+//
+//  Created by Rita Pinto on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VoiceCRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
